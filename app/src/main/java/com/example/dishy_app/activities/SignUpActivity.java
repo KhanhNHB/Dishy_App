f13914e830e3e058dd7c4a8f5ef5083bbc37205c
Khanh <khanhnhbse@gmail.com>
2019-10-03 19:22:29 +0700

    Add login, signup UI

---
+++ app/src/main/java/com/example/dishy_app/activities/SignUpActivity.java
@@ -0,0 +1,76 @@
+package com.example.dishy_app.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.method.PasswordTransformationMethod;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.dishy_app.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    private EditText txt_email, txt_password, txt_confirm_password;
+    private TextView btn_login_in;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        initView();
+        initData();
+    }
+
+    private void initView() {
+        txt_email = findViewById(R.id.txt_email);
+        txt_password = findViewById(R.id.txt_password);
+        txt_confirm_password = findViewById(R.id.txt_confirm_password);
+        btn_login_in = findViewById(R.id.btn_login_in);
+
+        txt_email.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    txt_email.setText("");
+                } else {
+                    txt_email.setText("Enter password");
+                }
+            }
+        });
+
+        txt_password.setTransformationMethod(new PasswordTransformationMethod());
+        txt_password.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    txt_password.setText("");
+                } else {
+                    txt_password.setText("Enter password");
+                }
+            }
+        });
+
+        txt_confirm_password.setTransformationMethod(new PasswordTransformationMethod());
+        txt_confirm_password.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    txt_confirm_password.setText("");
+                } else {
+                    txt_confirm_password.setText("Confirm password");
+                }
+            }
+        });
+    }
+
+    private void initData() {
+        btn_login_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+    }
+}
